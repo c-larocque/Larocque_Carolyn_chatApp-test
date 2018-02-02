27d7ffcd824fac98d21202f298d152aa68999528
Carolyn Larocque <clarocque1@live.ca>
2018-02-02 11:40:12 -0500

    add node functionality and install npm stuff

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express'); // require is the same thing as a php include -- basically means "include this"
+const app = express();
+const io = require('socket.io')(); // activate the chat plugin
+
+// serve up static files
+app.use(express.static('public'));
+
+// add routes
+app.use(require('./routes/index'));
+app.use(require('./routes/contact'));
+app.use(require('./routes/portfolio'));
+
+const server = app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
+
+io.attach(server);
+
+io.on('connection', socket => { // this is the same thing as function(socket) {...}
+  console.log('a user has connected!');
+
+  io.emit('chat message', { for : 'everyone', message : `${socket.id} is here!`});
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+
+    io.emit('disconnect message', `${socket.id} has left the building!`);
+  });
+});
